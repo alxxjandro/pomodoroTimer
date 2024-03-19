03ec3307c9dcc181e034647245feab211e96af4f
Alonso Alarcon <155941412+alxxjandro@users.noreply.github.com>
2024-03-19 13:41:59 -0600

    Added the basic design and theme button

---
+++ main.js
@@ -0,0 +1,8 @@
+const themeBtn = document.querySelector('#themeBtn');
+const body = document.querySelector('body')
+//const body2 = document.querySelector('#body2')
+
+themeBtn.addEventListener('click',()=>{
+    body.classList.toggle("prueba")
+    //body2.classList.toggle("prueba")
+})
